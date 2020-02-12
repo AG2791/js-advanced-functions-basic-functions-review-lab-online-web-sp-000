4dfc48498efec38f151e79a22b4459107d4bc6d1
AJ Guiles <ashiguiles@gmail.com>
2020-02-12 00:32:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // Your code here
+
+let saturdayFun = function(activity = "roller-skate"){
+  console.log(${activity})
+}
+
+
 let mondayWork = function( activity = 'go to office') {
   console.log(`"This Monday, I will ${activity}"`) 
 }  
 
-mondayWork()
 
  function wrapAdjective(visual = "*") {
    return function (aug1 = "special") {
