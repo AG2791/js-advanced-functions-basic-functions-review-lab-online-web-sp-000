c0c8cd5d9637781ba62c09bd4ac3e1e2d4684ba7
AJ Guiles <ashiguiles@gmail.com>
2020-02-12 00:33:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ let saturdayFun = function(activity = "roller-skate"){
   console.log(${activity})
 }
 
-
 let mondayWork = function( activity = 'go to office') {
   console.log(`"This Monday, I will ${activity}"`) 
 }  
